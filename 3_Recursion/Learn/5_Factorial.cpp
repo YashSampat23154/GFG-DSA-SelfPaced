aa8988424e2097f73fe3622175c89d7c24607426
Yash_Sampat <yashsampat23154@gmail.com>
2022-06-05 23:19:55 +0530

    Add files via upload

---
+++ 3_Recursion/Learn/5_Factorial.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+//struct Values{
+//int x;
+//};
+
+using namespace std;
+
+/*
+    Find Factorial of a number 
+*/
+ll fact(ll n){
+    if(n == 0) return 1;
+    return n*fact(n-1);
+}
+
+int main(){
+
+    fastio;
+
+    cout<<fact(5)<<endl;
+
+    return 0;
+}
