a4457c70c5a3a19a925d3fb4e73cd5d13086bbd5
Yash_Sampat <yashsampat23154@gmail.com>
2022-06-05 23:14:00 +0530

    To understand the basics of recursion.

---
+++ 1_Recursion Basics and Applications.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define rep(i,a,b) for(int i=a; i<b; i++)
+
+//struct Values{
+//int x;
+//};
+
+using namespace std;
+
+/*
+    Recursion : 
+    The process in which a function calls itself directly or indirectly is called recursion and the corresponding function is called as recursive function.
+
+    Typical Structure of Recursive Function : 
+
+    .... fun(......){
+
+        Base Cases
+        ........
+
+        Recusive Call(i.e. call to fun(.....))
+        With at least one change in parameter.
+
+    }
+    
+
+    Base Case : 
+    In recursion, a function calls itself and keep on calling itself. So there should be some point at which the calling should stop. To stop these callings we write some terminating conditions/recursion termination condition and those are called Base Cases.
+
+    Any program that can be written using recursion can also be implemented using iteration.
+
+    Advantages of Recursion: 
+    Recursion provides a clean and simple way to write code. Some problems are inherently recursive like tree traversals, Tower of Hanoi, etc. For such problems, it is preferred to write recursive code. We can write such codes also iteratively with the help of the stack data structure.
+
+    Disadvantage of Recursion: Note that both recursive and iterative programs have the same problem-solving powers, i.e., every recursive program can be written iteratively and vice versa. Recursive program has greater space requirements than iterative program as all functions will remain in the stack until the base case is reached. A recursive program also has greater time requirements because of function calls and return overhead.
+
+    Applications of Recursion : 
+    1. Many algorithm techniques are based on Recursion like : 
+    Dynamic Programming
+    Backtracking
+    Divide and Conquer (Binary Search, QuickSort, Merge Sort)
+    2.  Many problems inherently are recursive in nature like : 
+    Tower of Hanoi
+    DFS based traversals (DFS of Graph and Inorder/Preorder/Postorder traversals of tree)
+*/
+
+int main(){
+
+    fastio;
+
+    int t = 1;
+    cin>>t;
+
+    while(t--){
+
+        int n; 
+        cin>>n; 
+        
+        
+    }
+
+    return 0;
+}
