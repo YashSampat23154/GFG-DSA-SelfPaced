33b3aa9cfe2ba6f0c419b22448b46b6ad6a6a0ca
Yash_Sampat <yashsampat23154@gmail.com>
2023-01-10 07:43:10 +0530

    Practice Problem 3.

---
+++ 4_Array/Practice/3. Maximum Index.cpp
@@ -0,0 +1,109 @@
+/*
+    Given an array A[] of N positive integers. The task is to find the maximum of j - i subjected to the constraint of A[i] < A[j] and i < j.
+ 
+
+    Example 1:
+
+    Input:
+    N = 2
+    A[] = {1, 10}
+
+    Output:
+    1
+    
+    Explanation:
+    A[0]<A[1] so (j-i) is 1-0 = 1.
+    
+    
+    Example 2:
+
+    Input:
+    N = 9
+    A[] = {34, 8, 10, 3, 2, 80, 30, 33, 1}
+    
+    Output:
+    6
+    
+    Explanation:
+    In the given array A[1] < A[7] satisfying the required condition(A[i] < A[j]) thus giving the maximum difference of j - i which is 6(7-1).
+    
+
+    Your Task:
+    The task is to complete the function maxIndexDiff() which finds and returns maximum index difference. Printing the output will be handled by driver code. Return 0 in case no such index is found.
+
+    Expected Time Complexity: O(N)
+    Expected Auxiliary Space: O(N)
+
+    Constraints:
+    1 ≤ N ≤ 10^7
+    0 ≤ A[i] ≤ 10^9
+
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        int lmin[N], rmax[N];
+        
+        lmin[0] = A[0];
+        for(int i = 1; i<N; i++)
+            lmin[i] = min(A[i], lmin[i-1]);
+            
+        rmax[N-1] = A[N-1];
+        for(int i = N-2; i>=0 ; i--)
+            rmax[i] = max(A[i], rmax[i+1]);
+            
+        int i = 0, j = 0; 
+        int maxi = 0;
+        
+        while(i<N && j<N){
+            if(lmin[i] <= rmax[j]){
+                maxi = max(maxi, j-i);
+                j++;
+            }
+            else{
+                i++;
+            }
+        }
+        
+        return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
